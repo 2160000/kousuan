7abb93abb777cdab64354bd04cdf06a64e8e469e
2160000 <liu13001556486@163.com>
2024-12-11 20:57:07 +0800

    Add files via upload

---
+++ src/constants/questionTypes.ts
@@ -0,0 +1,8 @@
+import { QuestionTypeOption } from '../types';
+
+export const QUESTION_TYPES: QuestionTypeOption[] = [
+  { value: 'calculation', label: '算一算' },
+  { value: 'choice', label: '选一选' },
+  { value: 'truefalse', label: '判对错' },
+  { value: 'estimate', label: '估算' },
+];
